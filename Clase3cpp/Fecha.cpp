ce0c1096ea8e9bf42561d676a8427d2d683c6b22
Zait06 <zait-hdez@hotmail.com>
2020-02-05 07:23:28 -0600

    Constructor

--- Clase3cpp/Fecha.cpp
+++ Clase3cpp/Fecha.cpp
@@ -2,10 +2,19 @@
 #include <iostream>
 using namespace std;
 
-Fecha::Fecha(int dd, int mm, int aaaa){
-    mes=mm;
-    dia=dd;
-    anio=aaaa;
+Fecha::Fecha(int dd, int mm, int aaaa):dia(dd),mes(mm),anio(aaaa){
+    if((mes<1) || (mes>12)){
+        cout<<"Valor ilegal para el mes!"<<endl;
+        exit(1);
+    }
+    if(!(dia>=1 && dia<=31)){
+        cout<<"Valor ilegal para el dia"<<endl;
+        exit(1);
+    }
+    if(!(anio>=0 && anio<=2020)){
+        cout<<"Valor ilegal para el año"<<endl;
+        exit(1);
+    }
 }
 
 void Fecha::inicializaFecha(int dd, int mm, int aaaa){
