fd7d99bc0947d3ec1b2cea059a653c3322d302e7
Zait06 <zait-hdez@hotmail.com>
2020-02-05 07:28:24 -0600

    Constructor

--- Clase3cpp/programa2_3.cpp
+++ Clase3cpp/programa2_3.cpp
@@ -3,20 +3,6 @@
 using namespace std;
 
 int main(){
-    Fecha a, b, c(21,9,1973),f(4,3,1600);
-    int s=0;
-    b.inicializaFecha(17,6,2000);
+    Fecha a(21,9,2021);
     a.muestraFecha();
-    b.muestraFecha();
-    c.muestraFecha();
-    cout<<a.convierte()<<endl;
-    cout<<b.convierte()<<endl;
-    cout<<c.convierte()<<endl;
-    for(int i=1;i<2021;i++){
-        f.inicializaFecha(1,1,i);
-        if(f.leapyr()){
-            s++;
-        }
-    }
-    cout<<s<<endl;
 }
