55d6abe2d43ad6a7c0684f03ca0be8168034abb4
Zait06 <zait-hdez@hotmail.com>
2020-02-07 16:38:40 -0600

    Programa principal

--- Clase4cpp/prac_compo.cpp
+++ Clase4cpp/prac_compo.cpp
@@ -6,13 +6,12 @@ using namespace std;
 int main()
 {
     Rectangulo rectangulo1(2,3,5,1);
-    double ancho, alto;
 
     cout << "Calculando el área de un rectángulo dadas sus coordenadas en un plano cartesiano:" << endl;
+    
     rectangulo1.imprimeEsq();
 
-    alto = rectangulo1.obtieneSupIzq().obtenerY() - rectangulo1.obtieneInfDer().obtenerY();
-    ancho = rectangulo1.obtieneInfDer().obtenerX() - rectangulo1.obtieneSupIzq().obtenerX();
-    cout << "El área del rectángulo es = " << ancho*alto << endl;
+    cout << "El área del rectángulo es = " <<rectangulo1.obtieneArea()<< endl;
+
     return 0;
  }
