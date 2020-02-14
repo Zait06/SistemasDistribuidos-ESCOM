9edbcd86b9e3c272bee552f5a1a7df1b53bf2e7c
manuel-cmd <holaatodos987@gmail.com>
2020-02-14 08:10:43 -0600

    ejercicio 2

---
+++ Clase7cpp/programa7_2.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string cadena("ESCOM");
+    //cout<<cadena[6]<<endl;
+    cout<<cadena.at(4)<<endl;
+    char aCString[100];
+
+    /*string stringVariable;
+    getline(cin, stringVariable);
+    strcpy(aCString, stringVariable.c_str());
+
+    cout<<aCString<<endl;*/
+
+    return 0;
+}
