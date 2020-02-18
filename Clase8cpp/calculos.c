a26955b6215d5182f21e01c48129abd5936b7cce
Zait06 <zait-hdez@hotmail.com>
2020-02-18 09:30:37 -0600

    Palabra y calculos

---
+++ Clase8cpp/calculos.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]){
+    double n=strtol(argv[1], NULL, 10);
+    double i=0;
+    double seno=0;
+    double coseno=0;
+    double tangente=0;
+    double logaritmo=0;
+    double raiz=0;
+    while (i<n)
+    {
+        seno=sin(i);
+        coseno=cos(i);
+        tangente=tan(i);
+        logaritmo=log(i);
+        raiz=sqrt(i);
+        i++;
+    }
+
+    return 0;
+}
--- Clase8cpp/calculos.java
+++ Clase8cpp/calculos.java
@@ -1,15 +1,14 @@
 public class calculos {
     public static void main(String[] args) {
-        double i=1, seno=0,coseno=0,tangente=0,logaritmo=0,raiz=0;
+        double i=1,seno=0,coseno=0,tangente=0,logaritmo=0,raiz=0;
         // Scanner in = new Scanner(System.in);
         double n=Integer.parseInt(args[0]);
-        while (i<n)
-        {
-           seno= Math.sin(i);
-           coseno=Math.cos(i);
-           tangente=Math.tan(i);
-           logaritmo=Math.log(i);
-           raiz=Math.sqrt(i);
+        while (i<n){
+           seno+=Math.sin(i);
+           coseno+=Math.cos(i);
+           tangente+=Math.tan(i);
+           logaritmo+=Math.log(i);
+           raiz+=Math.sqrt(i);
            i++;
         }
     }  
