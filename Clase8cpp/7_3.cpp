871c1e23d7acca5fe553dee935fb5310aa172aa1
Zait06 <zait-hdez@hotmail.com>
2020-02-18 07:19:34 -0600

    Pruebas C y C++

---
+++ Clase8cpp/7_3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int n;
+    string npalabras;
+    string alfabeto  ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "Ingrese el número de palabras: ";
+    cin >> n;
+    string palabra="";
+    srand(time(NULL));
+    string letra;
+    // generar n “palabras” de tres
+    // letras cada una y de contenido aleatorio
+    for (int i=0; i<n; i++){
+        for(int j=0; j<3; j++){
+            letra=alfabeto[1+rand()%(25)];
+            palabra.append(letra);
+        }
+        //manteniendo un espacio en blanco de separación entre cada palabra.
+        palabra.append(" ");
+        npalabras.append( palabra);
+        palabra="";
+    }
+    //cout<< npalabras;
+    int total=0;
+    int pos=0;
+    int i=0;
+    //búsqueda de la subcadena “IPN”
+    while(i<npalabras.length()){
+        if(npalabras.find("IPN", pos)!= string::npos){
+            total++;
+            pos=npalabras.find("IPN", pos) + 2;
+        }else{
+            break;
+        }
+    }
+    cout << "numero de ocurrencias: " << total<<endl;   
+    return 0;
+}
---
+++ Clase8cpp/gigante.c
@@ -0,0 +1,37 @@
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    srand(time(NULL));
+    int n = 0, v= 0, i , j;
+    char* gigante = NULL;
+    char alfabeto[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char palabra[4];
+    char ipn[] = "IPN";
+    printf("Ingresa N: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+    	gigante = (char*)realloc(gigante, (i + 1) * 4);
+    for(int i = 0; i < 3; i++) {
+		int aleat = rand() % 25;
+		palabra[i] = alfabeto[aleat];
+	}
+	palabra[3] = ' ';
+	memcpy(gigante + 4 * i, palabra, 4);
+
+}
+    int SUB = strlen(ipn); //Tamaño de la subcadena 3
+	int CAD = strlen(gigante); //Tamaño de la cadenota
+	for(i = 0; i <= CAD-SUB; i++) {
+		for(j = 0; j < SUB; j++)
+			if(gigante[i + j] != ipn[j])
+				break;
+		if(j == SUB)
+			v++;
+	}
+    printf("Subcadena IPN encontrada: %d veces\n", v);
+    return 0;
+}
