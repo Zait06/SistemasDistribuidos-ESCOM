42bd58c96f8d41360fda7e8077efcee96e7d3dd4
Zait06 <zait-hdez@hotmail.com>
2020-02-18 19:03:14 -0600

    Palabras python

--- Clase8cpp/gigante.py
+++ Clase8cpp/gigante.py
@@ -1,28 +1,14 @@
 import sys
-import random
-
-def countWord(text):
-    text=text.upper().split()
-    s=0
-    for word in text:
-      if(word=="IPN"):
-        s=s+1
-    return s 
+from random import choice
+from string import ascii_uppercase 
 
 if __name__== "__main__":
     n=int(sys.argv[1])
     npalabras=""
-    alfabeto="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    palabra=""
-    
-    for i in range(n):
-        for j in range(3):
-            letra=alfabeto[1+random.randrange(25)]
-            palabra=palabra+letra
-        palabra=palabra+" "
-        npalabras=npalabras+palabra
-        palabra="";
 
-    total=countWord(npalabras)
+    for j in range(n):
+        npalabras+=(''.join(choice(ascii_uppercase) for i in range(3)))+" "
 
-    print("Numero de ocurrencias {}".format(total))
+    total=npalabras.count("IPN")
+
+    print("Numero de ocurrencias: {}".format(total))
