3cddd2d8c4ba7fa8ace86681039a8a35c4d69b4a
Pach15 <angel_andrade_98@hotmail.com>
2020-02-19 08:51:15 -0600

    Ejercicio6

---
+++ Clase9cpp/Ejercicio6.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+int varGlob=0;
+mutex mux;
+
+void incrementa(int n){
+	mux.lock();
+	for(int i=0;i<n;i++)
+        varGlob++;
+    mux.unlock();
+}
+
+void decrementa(int n){
+	mux.lock();
+    for(int i=0;i<n;i++)
+        varGlob--;
+    mux.unlock();
+}
+
+int main(int argc, char *argv[])
+{
+    int a=strtol(argv[1], NULL, 10);
+	thread th1(incrementa,a), th2(decrementa,a);
+	printf("Proceso principal espera que los hilos terminen\n");
+	th1.join();
+	th2.join();
+	printf("El hilo principal termina\n");
+    printf("VALOR: %d\n", varGlob);
+	exit(0);
+}
