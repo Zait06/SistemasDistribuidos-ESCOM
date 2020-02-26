427788e8e4a49e8ffb2fd3ced3d915554d29350a
Zait06 <zait-hdez@hotmail.com>
2020-02-26 07:41:18 -0600

    Ejercicio 4, inciso 2

--- Clase10cpp/Ejercicio4_2.cpp
+++ Clase10cpp/Ejercicio4_2.cpp
@@ -5,21 +5,15 @@
 #include <thread>
 using namespace std;
 
-void cpu(double n){
-    double i=0;
-    double seno=0;
-    double coseno=0;
-    double tangente=0;
-    double logaritmo=0;
-    double raiz=0;
-    while (i<n)
-    {
-        seno=sin(i);
-        coseno=cos(i);
-        tangente=tan(i);
-        logaritmo=log(i);
-        raiz=sqrt(i);
-        i++;
+#define numeroElementos 100000000
+
+void ram(long n){
+    int *arreglo, i;
+	arreglo = new int[numeroElementos];
+	for(i = 0; i < numeroElementos; i++)
+	arreglo[i] = 0;
+	for(i = 0; i < n; i++){
+	arreglo[rand()%numeroElementos] = rand();
     }
 }
 
@@ -30,7 +24,7 @@ int main(int argc, char *argv[])
         exit(0);
 	}
     int a=strtol(argv[1], NULL, 10);
-	thread th1(cpu,a), th2(cpu,a);
+	thread th1(ram,a), th2(ram,a);
 	th1.join();
 	th2.join();
 	exit(0);
