1a88c7bccc17e480e077262b0dedb68aaad82509
Zait06 <zait-hdez@hotmail.com>
2020-02-26 08:53:54 -0600

    Ejercicio 1,inciso B

--- Clase10cpp/1B.cpp
+++ Clase10cpp/1B.cpp
@@ -4,55 +4,54 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <cstring>
-
 using namespace std;
-char buffer[BUFSIZ];
-
+char buffer[1757600*4];
 int main(int argc, char *argv[])
 {
     int destino;
-
-    if(argc != 2){
-	    cout << "Forma de uso: " << argv[0] <<" nombre_del_archivo\n";
-	    exit(0);
-	}
-
-    string linea;
-    string alfabeto  ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	 if(argc != 2){
+	 cout << "Forma de uso: " << argv[0] <<" nombre_del_archivo\n";
+	 exit(0);
+	 }
+    long n;
     string npalabras;
+    string alfabeto  ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    n=1757600;
     string palabra="";
     srand(time(NULL));
     string letra;
-    int n;
-
-    cout<<"Introdusca n: ";
-    cin>>n;
-    for (int i=0; i<n; i++)
-    {
-        for(int j=0; j<3; j++)
-        {
+    // generar n “palabras” de tres
+    // letras cada una y de contenido aleatorio
+    for (long i=0; i<n; i++){
+        for(int j=0; j<3; j++){
             letra=alfabeto[1+rand()%(25)];
             palabra.append(letra);
         }
+        //manteniendo un espacio en blanco de separación entre cada palabra.
         palabra.append(" ");
         npalabras.append(palabra);
         palabra="";
     }
 
-    strcpy(buffer, npalabras.c_str( ));
 
-    //Abre un archivo para escritura, si no existe lo crea, si existe lo trunca, con permisos rw-
-    if((destino = open(argv[1], O_WRONLY|O_TRUNC|O_CREAT, 0666)) == -1)
-    {
-        perror(argv[1]);
-        exit(-1);
-    }
+
+
+
+	strcpy(buffer, npalabras.c_str( ));
+
+	 //Abre un archivo para escritura, si no existe lo crea, si existe lo trunca, con permisos rw-
+	 if((destino = open(argv[1], O_WRONLY|O_TRUNC|O_CREAT, 0666)) == -1)
+	 {
+	 perror(argv[1]);
+	 exit(-1);
+	 }
+    //write(destino, buffer, strlen(buffer));
     
-    for(int i=0; i<strlen(buffer); i++)
-    {
+    for(int i=0; i<1757600*4; i=i+4){
         write(destino, buffer+i, 4);
     }
-        
-    fsync(destino);
-    close(destino);
+
+
+	 fsync(destino);
+	 close(destino);
 }
