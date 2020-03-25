52c0b1833914b2d0b57ac3751cf88e712ee4997b
Zait06 <zait-hdez@hotmail.com>
2020-03-25 11:39:00 -0600

    Nuevo metodo con temporizador

---
+++ Practica05/client.cpp
@@ -0,0 +1,23 @@
+#include "Solicitud.h"
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    /*Los dos enteros se le deben pasar al cliente como parámetros en la línea de comandos.*/
+    if(argc != 4) {
+		printf("Forma de uso: %s ip_servidor num1 num2\n", argv[0]);
+		exit(0);
+	}
+
+    int num[2];
+	num[0] = atoi(argv[2]);
+	num[1] = atoi(argv[3]);
+	Solicitud s;
+	int res;
+	// memcpy(&res, s.doOperation(argv[1], 7200, 1, (char *)&num), 4);
+	memcpy(&res, s.doOperation(argv[1], 7200, 1, (char *)&num,2,500000), 4);
+    cout<<"Cliente Iniciado"<<endl;
+    cout<<"El resultado de "<<num[0]<<" + "<<num[1]<<" es: "<<res<<endl;
+    return 0;
+}
