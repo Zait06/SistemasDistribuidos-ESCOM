379b71ad4d489112df9c109400c23ba5a3836b0b
Zait06 <zait-hdez@hotmail.com>
2020-03-31 13:48:34 -0600

    Cajero

--- Practica06/server.cpp
+++ Practica06/server.cpp
@@ -7,16 +7,15 @@ int ndb=0;
 
 int main(){
     Respuesta resp(7200);
-    cout <<"Cajero iniciado iniciado....\n"<<endl;
+    cout <<"Cajero iniciado....\n"<<endl;
     while(1){
         struct mensaje* msj = resp.getRequest();
-        int moneda;
+        int moneda[1];
         memcpy(&moneda, &msj->arguments, sizeof(msj->arguments));
-        cout<<"Valor recibido: "<<moneda<<endl;
-        ndb+=moneda;
-        cout<<"\nValor de lo acumulado: "<<ndb<<endl;
-        int aux=ndb;
-        resp.sendReply((char*) &aux);
+        cout<<"Datos recibidos: "<<moneda[0]<<endl;
+        ndb+=moneda[0];
+        cout<<"\nEnviando respuesta: "<<ndb<<endl;
+        resp.sendReply((char*) &ndb);
     }
     return 0;
 }
