27eefd04eae5cf12c7dc9ffc6422047ceb81266b
Pach15 <angel_andrade_98@hotmail.com>
2020-05-07 11:56:46 -0500

    seguridad

---
+++ SDEP_Seguridad/Cliente/Solicitud.cpp
@@ -0,0 +1,50 @@
+#include "Solicitud.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+unsigned int contRequest = 0;
+
+Solicitud::Solicitud() {
+	socketlocal = new SocketDatagrama(0);
+}
+
+char *Solicitud::doOperation(char *IP, int puerto, int operationId, char *arguments) {
+	struct mensaje sms;
+	sms.messageType = 0;
+	sms.requestId = contRequest;
+	printf("\nContRequest: %u\n", contRequest);
+	sms.operationId = operationId;
+	memcpy(sms.arguments, arguments, 256);
+	PaqueteDatagrama p = PaqueteDatagrama((char *)&sms, sizeof(sms), IP, puerto);
+	//cout << "Direccion: " << p.obtieneDireccion() << endl;
+	//cout << "Puerto: " << p.obtienePuerto() << endl;
+	struct mensaje* smsprueba = (struct mensaje*)p.obtieneDatos();
+    //cout << "Registro: " <<smsprueba->arguments << endl;
+	//cout << "Registro: " <<sms.arguments << endl;
+	//cout << "Registro: " <<arguments << endl;
+	socketlocal->envia(p);
+	PaqueteDatagrama p1 = PaqueteDatagrama(65000);
+	int tam = socketlocal->recibeTimeout(p1, 2, 500);
+	int n = 1;
+
+	while (tam == -1 && n < 7) {
+		socketlocal->envia(p);
+		tam = socketlocal->recibeTimeout(p1, 2, 500);
+		n++;
+	}
+	
+	if (n == 7) {
+		cout << "Servidor no esta disponible, intente mas tarde." << endl;
+		exit(0);
+	}
+	else
+	{
+		socketlocal->~SocketDatagrama();
+		cout << "\nMensaje recibido" << endl;
+		cout << "Direccion: " << p1.obtieneDireccion() << endl;
+		cout << "Puerto: " << p1.obtienePuerto() << endl;
+		struct mensaje *msj = (struct mensaje *)p1.obtieneDatos();
+		contRequest++;
+		return (char *)msj->arguments;
+	}
+}
---
+++ SDEP_Seguridad/Servidor/server.cpp
@@ -0,0 +1,55 @@
+#include "Respuesta.h"
+#include "SocketDatagrama.h"
+#include <cstring>
+#include <iostream>
+#include <ctime>
+#include <fstream>//archivo
+#include <fcntl.h>//open
+
+#include <unistd.h>
+#include <sys/time.h>
+
+using namespace std;
+//Para cada registro recibido, el servidor deberá irlos guardando en un archivo, el cual será nuestra base
+//de datos.
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        cout<<"Forma de usa: nombre_programa nombre_archivo"<<endl;
+        exit(0);
+    }
+    int destino;
+    //Abre un archivo para escritura, si no existe lo crea, si existe lo trunca, con permisos rw-
+        if((destino = open(argv[1], O_WRONLY|O_TRUNC|O_CREAT, 0666)) == -1){
+            perror(argv[2]);
+            exit(-1);
+        }
+    int nbd=0;
+    Respuesta resp(7200);
+    cout << "Servidor iniciado....\n";
+    while (true) {
+        struct mensaje *msj = resp.getRequest();       
+        if(msj != NULL) {
+            char msjRecib[1024];
+            memcpy(msjRecib,&msj->arguments,34);
+	    
+	    timeval actual;
+            actual.tv_sec = 0;
+            actual.tv_usec = 0;  
+	    
+	    gettimeofday(&actual,NULL);
+                cout << actual.tv_sec << endl;
+                cout << actual.tv_usec << endl;
+               String regs += to_string(actual.tv_sec) + to_string(actual.tv_usec);
+
+	    write(regs.c_str(), regs.length()); 
+            write(destino,msjRecib, 34);
+            write(destino,"\n", 1);
+
+	    resp.sendReply((char *)&nbd);  
+        }else{//si es duplicado
+		cout << "Telefono ya registrado" << endl;
+                resp.sendReply((char *)&nbd);
+        }       
+    }
+    close(destino);
+}
