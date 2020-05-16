8499104568809318552c4c7dc4b7675983d67490
manuel-cmd <holaatodos987@gmail.com>
2020-05-16 00:12:27 -0500

    Cliente

--- SDEP_Balanceo/Cliente/client.cpp
+++ SDEP_Balanceo/Cliente/client.cpp
@@ -6,8 +6,7 @@
 #include <thread>  
 #include <fstream>//archivo
 
-void enviarMensaje(char *ipserv, int portserv, char *cadena, Solicitud s)
-{
+void enviarMensaje(char *ipserv, char portserv, char *cadena, Solicitud s){
     timeval res;
     memcpy(&res, s.doOperation(ipserv, portserv, 1, (char *)&cadena), sizeof(timeval));
 }
@@ -29,47 +28,35 @@ int main(int argc, char *argv[]){
     int res = 0;
     //leerá n registros de un archivo...
 	int n = atoi(argv[2]);
-    cout<<argv[3]<<" "<<argv[4]<<endl;
 	while (regactual < n){
         //irá leyendo cada registro del archivo...
         getline(archivo,s);
         memcpy(cadena,s.c_str(),s.size());
-        //cout<<cadena<<endl;//comprobación
-        
-        // y los manda en mensajes UDP(un registro en cada mensaje UDP) hacia el servidor.;
+        //cout<<cadena<<endl;//comprobación               
+               
+        // y los manda en mensajes UDP(un registro en cada mensaje UDP) hacia el servidor.;¿¿
         Solicitud s;
-        //timeval res;
-        //memcpy(&res, s.doOperation(argv[3], atoi(argv[4]), 1, (char *)&cadena), sizeof(timeval));
-        // timeval res;
-        //memcpy(&res, s.doOperation(argv[1], 7200, 1, (char *)&cadena), sizeof(timeval));
-
-        if(cadena[9] >= '0' || cadena[9]<= '3'){
-            cout<<cadena[9]<<endl;
-            //timeval res;
-            //memcpy(&res, s.doOperation(argv[3], atoi(argv[4]), 1, (char *)&cadena), sizeof(timeval));
-            thread t(enviarMensaje, argv[3], atoi(argv[4]), cadena, s);
-			t.detach();
-        }
-
-        if(cadena[9] >= '4' || cadena[9] <= '6'){
-            cout<<cadena[9]<<endl;
-            //timeval res;
-            //memcpy(&res, s.doOperation(argv[5], atoi(argv[6]), 1, (char *)&cadena), sizeof(timeval));
+        
+        int n = atoi(&cadena[9]);
+        cout<<"n: "<<n<<"  ";
+        if(n == 0 || n == 3 || n == 1 || n == 2){
+            cout<<"numero : "<<cadena<<" numero 10: "<<cadena[9]<<"  -1"<<endl;
             thread t(enviarMensaje, argv[5], atoi(argv[6]), cadena, s);
 			t.detach();
         }
-
-        if(cadena[9] >= '7' || cadena[9] <= '9'){
-            cout<<cadena[9]<<endl;
-            //timeval res;
-            //memcpy(&res, s.doOperation(argv[7], atoi(argv[8]), 1, (char *)&cadena), sizeof(timeval));
-            thread t(enviarMensaje, argv[7], atoi(argv[8]), cadena, s);
+        /*if(n == 4 || n == 6 || n == 5){
+            cout<<"numero : "<<cadena<<" numero 10: "<<cadena[9]<<"  -2"<<endl;
+            thread t(enviarMensaje, argv[5], atoi(argv[6]), cadena, s);
 			t.detach();
         }
-		
-		//cout <<"Client.cpp:"<<res.tv_sec<<" : "<< res.tv_usec << endl;
+        if(n == 7 || n == 9 || n == 8){
+            cout<<"numero : "<<cadena<<" numero 10: "<<cadena[9]<<"  -3"<<endl;
+            thread t(enviarMensaje, argv[7], atoi(argv[8]), cadena, s);
+			t.detach();
+        }*/
+        //cout <<"Client.cpp:"<<res.tv_sec<<" : "<< res.tv_usec << endl;
 		regactual++;
-        //sleep(1);
+        sleep(1);
 	}
 	return 0;
 }
